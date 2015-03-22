62ea06585c532a9dcf635029c9a9e4173e45b75a
Zyanya <zeta_val@hotmail.com>
2015-03-21 18:24:43 -0600

    HomeActivity

--- HackMTY20152/app/src/main/java/itesm/mx/hackmty2015/HomeActivity.java
+++ HackMTY20152/app/src/main/java/itesm/mx/hackmty2015/HomeActivity.java
@@ -1,18 +1,59 @@
 package itesm.mx.hackmty2015;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class HomeActivity extends ActionBarActivity {
+
+    // Declaración de Variables
+    ListView eventLV;
+    ArrayList eventos = new ArrayList<String>();
+    HashMap<int, Object> mapper = new HashMap();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Referencias a objetos de interface
+        eventLV = (ListView) findViewById(R.id.eventLV);
+
+        AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+
+            }
+        };
+        eventLV.setOnItemClickListener(itemListener);
+
+        eventos.add("Fiesta"); eventos.add("Despensa Familia") ; eventos.add("Ida a Laredo");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.evento_row,R.id.rowTV,eventos);
+
+        eventLV.setAdapter(adapter);
     }
+
+    public void onClickAddList(View v){
+        Intent addListIntent = new Intent();
+
+
+    }
+
 
 
     @Override
